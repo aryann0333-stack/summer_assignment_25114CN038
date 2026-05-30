780758cddee532daca1a2e876648478e406e13c3
aryann0333-stack <aryann.0333@gmail.com>
2026-05-30 10:14:18 +0530

    Day03 ques 9 done

---
+++ day_03/problem1.c
@@ -0,0 +1,27 @@
+// program to check  whether number is prime or not
+
+#include <stdio.h>
+
+int main()
+{
+    int num, i, flag = 1;
+    printf("Enter a required number to check: ");
+    scanf("%d", &num);
+
+    for (i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            flag = 0;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("Enterd number is not prime");
+    }
+    else
+
+        printf("Ennterd number is prime");
+
+    return 0;
+}
