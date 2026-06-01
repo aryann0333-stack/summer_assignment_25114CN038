4ba00392cebdbc09d26738b8f5b80ea1d2b4e929
aryann0333-stack <aryann.0333@gmail.com>
2026-06-01 12:39:58 +0530

    Day 05 uploaded

---
+++ Day_05/problem19.c
@@ -0,0 +1,19 @@
+//  program to Print factors of a number.
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+
+    printf("Enter a number:");
+    scanf("%d", &num);
+
+    printf("The factors of a number are \n");
+
+    for (i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
