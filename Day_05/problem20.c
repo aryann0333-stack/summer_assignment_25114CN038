6983478d513754fcdd9d0fba314e95ed1881eb7c
aryann0333-stack <aryann.0333@gmail.com>
2026-06-01 13:07:07 +0530

    Day 05 uploaded

---
+++ Day_05/problem20.c
@@ -0,0 +1,32 @@
+//  program to Find largest prime factor.
+#include <stdio.h>
+
+int main()
+{
+    int num, prime, i, j, largest = 0;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    for (i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            prime = 1;
+            for (j = 2; j < i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    prime = 0;
+                    break;
+                }
+            }
+            if (prime == 1)
+            {
+                largest = i;
+            }
+        }
+    }
+    printf("Largest prime number is :%d ", largest);
+    return 0;
+}
