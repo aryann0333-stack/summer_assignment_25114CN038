977064f6f85266089c990c30af4e765b180405c7
aryann0333-stack <aryann.0333@gmail.com>
2026-06-08 14:29:57 +0530

    Day 08 uploaded

---
+++ Day_08/problem30.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class problem30 {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Enter length of pyramid: ");
+            int leng = sc.nextInt();
+
+            // outer loop
+
+            for (int i = 1; i <= leng; i++) {
+
+                // inner loop for printing
+
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(+j);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
