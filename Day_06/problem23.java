bbf0d07f206939f5e6280c65ee2be8e89a5c1121
aryann0333-stack <aryann.0333@gmail.com>
2026-06-09 15:19:42 +0530

    Day 06 uploaded

--- Day_06/problem23.java
+++ Day_06/problem23.java
@@ -1,8 +1,21 @@
-//  program to Count set bits in a number
-#include <stdio.h>
 
-int main()
-{
+//  program to Count set bits in a number.
 
-    return 0;
+import java.util.Scanner;
+
+public class problem23 {
+        public static void main(String[] args) {
+                try (Scanner sc = new Scanner(System.in)) {
+
+                        System.out.print("Enter a number: ");
+                        int num = sc.nextInt();
+                        int count = 0;
+
+                        while (num > 0) {
+                                count += (num & 1);
+                                num = num >> 1;
+                        }
+                        System.out.println("Number of set  bits are : " + count);
+                }
+        }
 }
