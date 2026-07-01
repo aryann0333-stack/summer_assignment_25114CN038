12b748a2ae634659aa5723275e0155477afe28bb
aryann0333-stack <aryann.0333@gmail.com>
2026-07-01 07:26:45 +0530

    Day 20 uploaded

---
+++ Day_20/problem79.java
@@ -0,0 +1,40 @@
+
+// Program to Find Row-wise Sum
+import java.util.Scanner;
+
+public class problem79 {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Enter number of rows: ");
+            int rows = sc.nextInt();
+
+            System.out.print("Enter number of columns: ");
+            int cols = sc.nextInt();
+
+            int[][] matrix = new int[rows][cols];
+
+            System.out.println("Enter matrix:");
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.println("Row-wise Sum:");
+
+            for (int i = 0; i < rows; i++) {
+
+                int sum = 0;
+
+                for (int j = 0; j < cols; j++) {
+                    sum += matrix[i][j];
+                }
+
+                System.out.println("Row " + (i + 1) + " = " + sum);
+            }
+        }
+    }
+}
