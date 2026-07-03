a0a8d601b140dec2f82176f05c5aa8e405874244
aryann0333-stack <aryann.0333@gmail.com>
2026-07-03 08:41:49 +0530

    Day  23 uploaded

---
+++ Day_23/problem91.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problem91 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        str1 = str1.toLowerCase().replace(" ", "");
+        str2 = str2.toLowerCase().replace(" ", "");
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Strings are not Anagrams");
+        } else {
+
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("Strings are Anagrams");
+            } else {
+                System.out.println("Strings are not Anagrams");
+            }
+        }
+
+        sc.close();
+    }
+}
